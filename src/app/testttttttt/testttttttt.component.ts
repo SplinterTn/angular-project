84aa49af15debfffd4b6ff499cbeafa08001fc01
xRayzox <wael.hcine@esprit.tn>
2025-04-28 16:29:21 +0100

    tttttttttttttttttttttttttaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/app/testttttttt/testttttttt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testttttttt',
+  standalone: true,
+  imports: [],
+  templateUrl: './testttttttt.component.html',
+  styleUrl: './testttttttt.component.scss'
+})
+export class TesttttttttComponent {
+
+}
